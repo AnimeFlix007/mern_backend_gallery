2de43dd4ac93f15f4e74f6cc3476327b54ef11e0
devjitbose111 <devjitbose111@gmail.com>
2022-11-10 09:30:39 +0530

    provided another origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://mern-gallery-anime-app.onrender.com"],
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
   })
